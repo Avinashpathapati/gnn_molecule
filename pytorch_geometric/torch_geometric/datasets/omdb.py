16775672b976aaeeaa3f905eaff07e49ba3dd325
A.Pathapati@student.rug.nl <s3754715@peregrine.hpc.rug.nl>
2020-02-29 14:18:03 +0100

    added omdb changes missed last commit

--- pytorch_geometric/torch_geometric/datasets/omdb.py
+++ pytorch_geometric/torch_geometric/datasets/omdb.py
@@ -159,6 +159,7 @@ class OMDB(InMemoryDataset):
                 bond_idx += 2 * [self.bonds[bond.GetBondType()]]
 
             edge_index = torch.tensor([row, col], dtype=torch.long)
+            print(edge_index)
             edge_attr = F.one_hot(torch.tensor(bond_idx),
                                    num_classes=len(self.bonds)).to(torch.float)
             edge_index, edge_attr = coalesce(edge_index, edge_attr, N, N)
