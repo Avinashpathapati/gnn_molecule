eb98364f7358b9f56a3417083f8b951bda7d858e
Sanddhya Venkatram <avinashraj316@gmail.com>
2020-04-05 17:23:52 +0530

    removed print stat

--- pytorch_geometric/examples/omdb_gcn.py
+++ pytorch_geometric/examples/omdb_gcn.py
@@ -50,12 +50,6 @@ class Net(torch.nn.Module):
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
-        print(edge_index.shape)
-        print(x.shape)
-        print(data.num_features)
-        print(data.num_edges)
-        print(x)
-        print(edge_index)
         x = F.relu(self.conv1(x, edge_index))
         x = F.dropout(x, training=self.training)
         x = self.conv2(x, edge_index)
