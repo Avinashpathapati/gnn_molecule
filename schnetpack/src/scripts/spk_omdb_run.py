927c80743e2d5a883bf0aaa279edc35bc994b1a9
Sanddhya Venkatram <avinashraj316@gmail.com>
2020-04-16 14:39:50 +0530

    random seed

--- schnetpack/src/scripts/spk_omdb_run.py
+++ schnetpack/src/scripts/spk_omdb_run.py
@@ -121,7 +121,7 @@ def main(args):
 	#building model and dataset
 	spk.utils.spk_utils.set_random_seed(None)
 	device = torch.device("cuda" if args.cuda else "cpu")
-	environment_provider = spk.environment.AseEnvironmentProvider(cutoff=10.0)
+	environment_provider = spk.environment.AseEnvironmentProvider(cutoff=5.0)
 	omdb = './omdb'
 	if args.mode == "train":
 		if not os.path.exists('omdb'):
