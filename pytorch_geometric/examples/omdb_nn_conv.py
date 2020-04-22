eb4b376c83d3f51db95e290d1c1fafc53fae5866
Sanddhya Venkatram <avinashraj316@gmail.com>
2020-04-22 23:04:41 +0530

    changed first conv to 64

--- pytorch_geometric/examples/omdb_nn_conv.py
+++ pytorch_geometric/examples/omdb_nn_conv.py
@@ -78,9 +78,10 @@ class Net(torch.nn.Module):
         super(Net, self).__init__()
         self.lin0 = torch.nn.Linear(dataset.num_features, dim)
 
+        nn0 = Sequential(Linear(2, 64), ReLU(), Linear(64, 2*dim*dim))
         nn = Sequential(Linear(2, 64), ReLU(), Linear(64, dim * dim))
         #nn = Sequential(Linear(5, dim * dim))
-        self.conv0 = NNConv(dim, 2*dim, nn, aggr='mean')
+        self.conv0 = NNConv(dim, 2*dim, nn0, aggr='mean')
         self.gru0 = GRU(2*dim, dim)
 
         self.conv = NNConv(dim, dim, nn, aggr='mean')
