6639fb267b4ec1031ee0a2e4ea59cf7c0c8ab1f1
Sanddhya Venkatram <avinashraj316@gmail.com>
2020-05-21 22:12:55 +0530

    print

--- pytorch_geometric/torch_geometric/nn/models/dimenet.py
+++ pytorch_geometric/torch_geometric/nn/models/dimenet.py
@@ -341,11 +341,11 @@ class DimeNet(torch.nn.Module):
         # Interaction blocks.
         for interaction_block, output_block in zip(self.interaction_blocks,
                                                    self.output_blocks[1:]):
-            x = interaction_block(x, rbf, sbf, idx_kj, idx_ji)
             print('-------aaa')
             print(x.shape)
             print(rbf.shape)
             print(i.shape)
+            x = interaction_block(x, rbf, sbf, idx_kj, idx_ji)
             P += output_block(x, rbf, i)
 
         return P.sum(dim=0) if batch is None else scatter(P, batch, dim=0)
