6c19a76aa82a36a72decf14fbeb0c0e237464d21
Sanddhya Venkatram <avinashraj316@gmail.com>
2020-05-30 14:29:15 +0530

    resnet connection instead of gated unit

--- pytorch_geometric/examples/omdb_nn_conv_resnet.py
+++ pytorch_geometric/examples/omdb_nn_conv_resnet.py
@@ -3,22 +3,62 @@ import os
 import matplotlib.pyplot as plt
 import torch
 import torch.nn.functional as F
-import numpy as np
 from torch.nn import Sequential, Linear, ReLU, GRU
 
 import torch_geometric.transforms as T
 from torch_geometric.datasets import OMDBXYZ
-from torch_geometric.nn import NNConv, Set2Set, GCNConv
+from torch_geometric.nn import NNConv, Set2Set
 from torch_geometric.data import DataLoader
 from torch_geometric.utils import remove_self_loops
+import logging
 
 target = 0
 dim = 32
 
+model_path = os.path.join(os.getcwd(),'model')
+if not os.path.exists(model_path):
+    os.makedirs('model')
+
+class MyTransform(object):
+    def __call__(self, data):
+        # Specify target.
+        return data
+
+
+class Complete(object):
+    def __call__(self, data):
+        device = data.edge_index.device
+
+        row = torch.arange(data.num_nodes, dtype=torch.long, device=device)
+        col = torch.arange(data.num_nodes, dtype=torch.long, device=device)
+
+        row = row.view(-1, 1).repeat(1, data.num_nodes).view(-1)
+        col = col.repeat(data.num_nodes)
+        edge_index = torch.stack([row, col], dim=0)
+
+        edge_attr = None
+        if data.edge_attr is not None:
+            idx = data.edge_index[0] * data.num_nodes + data.edge_index[1]
+            size = list(data.edge_attr.size())
+            size[0] = data.num_nodes * data.num_nodes
+            edge_attr = data.edge_attr.new_zeros(size)
+            edge_attr[idx] = data.edge_attr
+
+        edge_index, edge_attr = remove_self_loops(edge_index, edge_attr)
+        data.edge_attr = edge_attr
+        data.edge_index = edge_index
+
+        return data
+
 
 path = osp.join(osp.dirname(osp.realpath(__file__)), '..', 'data', 'OMDB')
-#transform = T.Compose([MyTransform(), Complete(), T.Distance(norm=False)])
-dataset = OMDBXYZ(path).shuffle()
+transform = T.Compose([MyTransform(), Complete(), T.Distance(norm=False), 
+    T.RandomTranslate(0.01), T.RandomRotate(15, axis=0), T.RandomRotate(15, axis=1), T.RandomRotate(15, axis=2)])
+dataset = OMDBXYZ(path,transform=transform).shuffle()
+
+print(len(dataset))
+dataset = dataset[dataset.data.y > 0]
+print(len(dataset))
 
 # Normalize targets to mean = 0 and std = 1.
 mean = dataset.data.y.mean(dim=0, keepdim=True)
@@ -31,6 +71,7 @@ train_dataset = dataset[:9000]
 val_dataset = dataset[9000:10000]
 test_dataset = dataset[10000:]
 
+
 val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=32, shuffle=True)
@@ -39,11 +80,10 @@ test_loader = DataLoader(test_dataset, batch_size=32, shuffle=True)
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-
         self.lin0 = torch.nn.Linear(dataset.num_features, dim)
 
-        nn0 = Sequential(Linear(1, 64), ReLU(), Linear(64, 2*dim*dim))
-        nn = Sequential(Linear(1, 64), ReLU(), Linear(64, dim * dim))
+        nn0 = Sequential(Linear(2, 64), ReLU(), Linear(64, 2*dim*dim))
+        nn = Sequential(Linear(2, 64), ReLU(), Linear(64, dim * dim))
         #nn = Sequential(Linear(5, dim * dim))
         self.conv0 = NNConv(dim, 2*dim, nn0, aggr='mean')
         self.gru0 = GRU(2*dim, dim)
@@ -54,24 +94,32 @@ class Net(torch.nn.Module):
         self.set2set = Set2Set(dim, processing_steps=1)
         self.lin1 = torch.nn.Linear(2 * dim, dim)
         self.lin2 = torch.nn.Linear(dim, 1)
-        #self.avp = torch.nn.AdaptiveAvgPool1d(1)
-        # self.conv1 = ChebConv(data.num_features, 16, K=2)
-        # self.conv2 = ChebConv(16, data.num_features, K=2)
-
-        #self.reg_params = self.conv1.parameters()
-        #self.non_reg_params = self.conv2.parameters()
 
     def forward(self, data):
-        out = F.relu(self.lin0(data.x))
-        # h = out.unsqueeze(0)
-        for i in range(8):
-            m = F.relu(self.conv(out, data.edge_index, data.edge_attr))
-            out = out + m
+        #print('---------------')
+        #print(data.x.shape)
+        out1 = F.relu(self.lin0(data.x))
+        #print(out.shape)
+        h = out.unsqueeze(0)
 
-        # out = self.set2set(out, torch.zeros(1, dtype=torch.long, device=device))
-        out = torch.mean(out, axis=0)
-        # out = F.relu(self.lin1(out))
-        out = F.relu(self.lin2(out))
+        for i in range(8):
+            if i==0:
+                m = F.relu(self.conv(out1, data.edge_index, data.edge_attr))
+            else:
+                m = F.relu(self.conv(out, data.edge_index, data.edge_attr))
+
+            out, h = self.gru(m.unsqueeze(0), h)
+            out = out.squeeze(0)
+
+        #print(out.shape)
+        out = F.relu(out) + out1
+        out = self.set2set(out, data.batch)
+        #print(out.shape)
+        out = F.relu(self.lin1(out))
+        #print(out.shape)
+        out = self.lin2(out)
+        #print(out.shape)
+        #print('-----------------')
         return out.view(-1)
 
 
@@ -80,7 +128,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = Net().to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min',
-                                                       factor=0.7, patience=25,
+                                                       factor=0.8, patience=25,
                                                        min_lr=1e-6)
 
 
@@ -88,17 +136,12 @@ def train(epoch):
     model.train()
     loss_all = 0
 
-    for data_batch in train_loader:
-        #to extract data object from batch
-        data_batch = data_batch.to(device)
-        data_list = data_batch.to_data_list()
+    for data in train_loader:
+        data = data.to(device)
         optimizer.zero_grad()
-        loss_batch = 0
-        for data in data_list:
-            loss = F.mse_loss(model(data), data.y)
-            loss.backward()
-            loss_batch += loss.item()
-        loss_all += loss_batch
+        loss = F.mse_loss(model(data), data.y)
+        loss.backward()
+        loss_all += loss.item() * data.num_graphs
         optimizer.step()
     return loss_all / len(train_loader.dataset)
 
@@ -107,19 +150,16 @@ def test(loader):
     model.eval()
     error = 0
 
-    for data_batch in loader:
-        data_batch = data_batch.to(device)
-        data_list = data_batch.to_data_list()
-        for data in data_list:
-            error += (model(data) * std - data.y * std).abs().sum().item()  # MAE
-    
+    for data in loader:
+        data = data.to(device)
+        error += (model(data) * std - data.y * std).abs().sum().item()  # MAE
     return error / len(loader.dataset)
 
 
 def plot_results(epochs, train_loss, val_loss, test_loss):
     
-    if osp.isfile('./gcnconv.png'):
-        os.remove('./gcnconv.png') 
+    if osp.isfile('./nnconv.png'):
+        os.remove('./nnconv.png') 
     plt.figure()
     plt.plot(epochs, train_loss, label='Train')
     plt.plot(epochs, val_loss, label='Validation')
@@ -128,11 +168,12 @@ def plot_results(epochs, train_loss, val_loss, test_loss):
     plt.xlabel('epochs')
     plt.title('Validation Test Error Results')
     plt.legend(['Train', 'Validation', 'Test'], loc='upper left')
-    plt.savefig('./gcnconv.png')
+    plt.savefig('./nnconv.png')
     plt.close()
 
 
 best_val_error = None
+best_test_error = None
 train_loss = []
 val_loss = []
 test_loss = []
@@ -146,7 +187,14 @@ for epoch in range(1, 501):
     test_error = test(test_loader)
     test_loss.append(test_error)
 
-    print('Epoch: {:03d}, LR: {:7f}, Loss: {:.7f}, Validation MAE: {:.7f}, '
-          'Test MAE: {:.7f}'.format(epoch, lr, loss, val_error, test_error))
+    if best_val_error is None or val_error <= best_val_error:
+        best_test_error = test_error
+        best_val_error = val_error
+        torch.save(model, os.path.join(model_path, "best_model"))
+
+    
+
+    logging.warning('Epoch: {:03d}, LR: {:7f}, Loss: {:.7f}, Validation MAE: {:.7f}, '
+          'Test MAE: {:.7f},Best Test MAE: {:.7f}'.format(epoch, lr, loss, val_error, test_error, best_test_error))
 
 plot_results(range(1, 501), train_loss, val_loss, test_loss)
