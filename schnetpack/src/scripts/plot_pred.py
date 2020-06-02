f7d1a4b8f0c407469105da48315b25d7514b177a
Sanddhya Venkatram <avinashraj316@gmail.com>
2020-06-02 23:30:13 +0530

    plot print change

--- schnetpack/src/scripts/plot_pred.py
+++ schnetpack/src/scripts/plot_pred.py
@@ -23,8 +23,16 @@ from schnetpack.utils import (
     get_output_module
 )
 
+atom_id_input_arr = []
+atom_output = []
 
-def printnorm(self, input, output):
+
+def inputExtract(self, input, output):
+	
+	print(input[0].shape)
+	print('---------hi')
+
+def outputExtract(self, input, output):
 	
 	print(input[0].shape)
 	print('---------')
@@ -40,7 +48,9 @@ def main(args):
 	environment_provider = spk.environment.AseEnvironmentProvider(cutoff=5.0)
 
 	sch_model = torch.load(os.path.join(args.model_path, 'best_model'), map_location=torch.device(device))
-	sch_model.output_modules[0].out_net[1].out_net[1].register_forward_hook(printnorm)
+
+	sch_model.representation.embedding.register_forward_hook(inputExtract)
+	sch_model.output_modules[0].out_net[1].out_net[1].register_forward_hook(outputExtract)
 
 	for name, module in sch_model.named_modules():
 		print(name)
